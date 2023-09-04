9bb34ad168b03ed55fb5240e79799cb0aeb2ef8a
Bishal <bishalbera2@gmail.com>
2023-09-04 07:11:10 +0530

    made a function for routing

---
+++ lib/core/services/router.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+
+Route<dynamic> generateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case OnBoardingScreen.routeName:
+      return _pageBuilder(
+        (_) => const OnBoardingScreen(),
+        settings: settings,
+      );
+    default:
+      return _pageBuilder(
+        (_) => const PageUnderConstruction(),
+        settings: settings,
+      );
+  }
+}
+
+PageRouteBuilder<dynamic> _pageBuilder(
+  Widget Function(BuildContext) page, {
+  required RouteSettings settings,
+}) {
+  return PageRouteBuilder(
+    settings: settings,
+    transitionsBuilder: (_, animation, __, child) => FadeTransition(
+      opacity: animation,
+      child: child,
+    ),
+    pageBuilder: (context, _, __) => page(context),
+  );
+}
