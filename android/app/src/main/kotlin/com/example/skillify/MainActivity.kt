f5315b4c4f05fc01df9bf25b45695769bf9c4400
Bishal <bishalbera2@gmail.com>
2023-09-04 06:54:37 +0530

    app made with starter code

---
+++ android/app/src/main/kotlin/com/example/skillify/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.skillify
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:skillify/core/res/colours.dart';
+import 'package:skillify/core/res/fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Skillify',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(accentColor: Colours.primaryColour),
+        useMaterial3: true,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        fontFamily: Fonts.poppins,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.transparent,
+        ),
+      ),
+      onGenerateRoute: generateRoute,
+    );
+  }
+}
