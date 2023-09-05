7d7fc64740102de785d704fdc991b5a582816fed
Bishal <bishalbera2@gmail.com>
2023-09-05 09:15:01 +0530

    completed entities and repo of domain layer of auth

---
+++ lib/src/auth/domain/entities/user.dart
@@ -0,0 +1,50 @@
+import 'package:equatable/equatable.dart';
+
+class LocalUser extends Equatable {
+  const LocalUser({
+    required this.uid,
+    required this.email,
+    required this.name,
+    required this.points,
+    required this.groupIds,
+    required this.enrolledCourseIds,
+    required this.following,
+    required this.followers,
+    this.profilePic,
+    this.bio,
+  });
+
+  const LocalUser.empty()
+      : this(
+          uid: '',
+          email: '',
+          points: 0,
+          name: '',
+          profilePic: '',
+          bio: '',
+          groupIds: const [],
+          enrolledCourseIds: const [],
+          followers: const [],
+          following: const [],
+        );
+
+  final String uid;
+  final String email;
+  final String name;
+  final String? profilePic;
+  final String? bio;
+  final int points;
+  final List<String> groupIds;
+  final List<String> enrolledCourseIds;
+  final List<String> following;
+  final List<String> followers;
+
+  @override
+  List<Object?> get props => [uid, email];
+
+  @override
+  String toString() {
+    return ' LocalUser(uid: $uid, email: $email, name: $name, '
+        'bio: $bio, points: $points,)';
+  }
+}
---
+++ lib/src/auth/domain/repos/auth_repo.dart
@@ -0,0 +1,25 @@
+import 'package:skillify/core/enums/update_user.dart';
+import 'package:skillify/core/utils/typedef.dart';
+import 'package:skillify/src/auth/domain/entities/user.dart';
+
+abstract class AuthRepo {
+  const AuthRepo();
+
+  ResultFuture<void> forgotPassword(String email);
+
+  ResultFuture<LocalUser> signIn({
+    required String email,
+    required String password,
+  });
+
+  ResultFuture<void> signUp({
+    required String email,
+    required String name,
+    required String password,
+  });
+
+  ResultFuture<void> updateUser({
+    required UpdateUserAction action,
+    dynamic userData,
+  });
+}
