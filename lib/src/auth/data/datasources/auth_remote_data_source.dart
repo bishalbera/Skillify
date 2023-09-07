d8c84ca547779bca2c273cd6aa4975da7aec4ece
Bishal <bishalbera2@gmail.com>
2023-09-07 15:22:58 +0530

    auth remote data source impl done in data  layer

--- lib/src/auth/data/datasources/auth_remote_data_source.dart
+++ lib/src/auth/data/datasources/auth_remote_data_source.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:skillify/core/enums/update_user.dart';
 import 'package:skillify/core/errors/exceptions.dart';
@@ -56,20 +60,143 @@ class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
   }
 
   @override
-  Future<LocalUserModel> signIn(
-      {required String email, required String password}) {}
+  Future<LocalUserModel> signIn({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final result = await _client.auth.signInWithPassword(
+        email: email,
+        password: password,
+      );
 
-  @override
-  Future<void> signUp(
-      {required String email, required String name, required String password}) {
-    // TODO: implement signUp
-    throw UnimplementedError();
+      final session = result.session;
+
+      final user = result.user;
+      if (user == null) {
+        throw const ServerException(
+          message: 'Please try again later',
+          statusCode: 'Unknown Error',
+        );
+      }
+
+      var userData = await _getUserData(user.id);
+      if (userData.isNotEmpty) {
+        return LocalUserModel.fromMap(userData.values as DataMap);
+      }
+
+      await _setUserData(user, email);
+      userData = await _getUserData(user.id);
+      return LocalUserModel.fromMap(userData.values as DataMap);
+    } on AuthException catch (e) {
+      throw ServerException(
+        message: e.message ?? 'Error Occured',
+        statusCode: e.statusCode,
+      );
+    } catch (e, s) {
+      debugPrintStack(stackTrace: s);
+      throw ServerException(
+        message: e.toString(),
+        statusCode: '505',
+      );
+    }
   }
 
   @override
-  Future<void> updateUser({required UpdateUserAction action, userData}) {
-    // TODO: implement updateUser
-    throw UnimplementedError();
+  Future<void> signUp({
+    required String email,
+    required String name,
+    required String password,
+  }) async {
+    try {
+      final res = await _client.auth.signUp(email: email, password: password);
+      await _client.from('users').upsert({
+        'name': name,
+      });
+      await _setUserData(res.user, email);
+    } on AuthException catch (e) {
+      throw ServerException(
+        message: e.message ?? 'Error Occured',
+        statusCode: e.statusCode,
+      );
+    } catch (e, s) {
+      debugPrintStack(stackTrace: s);
+      throw ServerException(
+        message: e.toString(),
+        statusCode: '505',
+      );
+    }
+  }
+
+  @override
+  Future<void> updateUser({
+    required UpdateUserAction action,
+    dynamic userData,
+  }) async {
+    try {
+      switch (action) {
+        case UpdateUserAction.profilePic:
+          final imagePath = 'profile_pics/${_client.auth.currentUser?.id}';
+
+          await _dbClient.createBucket('profile_pics');
+          await _dbClient.updateBucket(
+            'profile_pics',
+            const BucketOptions(public: true),
+          );
+          await _dbClient.from('profile_pics').upload(
+                imagePath,
+                userData as File,
+              );
+          final url = _dbClient.from('profile_pics').getPublicUrl(imagePath);
+
+          await _client.auth.updateUser(
+            UserAttributes(
+              data: {
+                'profilePic': url,
+              },
+            ),
+          );
+          await _updateUserData({'profilePic': url});
+
+        case UpdateUserAction.displayName:
+          await _updateUserData({
+            'name': userData as String,
+          });
+        case UpdateUserAction.email:
+          await _client.auth
+              .updateUser(UserAttributes(email: userData as String));
+          await _updateUserData({
+            'email': userData,
+          });
+        case UpdateUserAction.password:
+          if (_client.auth.currentUser?.email == null) {
+            throw const ServerException(
+              message: 'User does not exist',
+              statusCode: 'Insufficient Permission',
+            );
+          }
+          final newData = jsonDecode(userData as String) as DataMap;
+          await _client.auth.updateUser(
+            UserAttributes(
+              password: newData['newPassword'] as String,
+            ),
+          );
+
+        case UpdateUserAction.bio:
+          await _updateUserData({'bio': userData as String});
+      }
+    } on AuthException catch (e) {
+      throw ServerException(
+        message: e.message ?? 'Error Occured',
+        statusCode: e.statusCode,
+      );
+    } catch (e, s) {
+      debugPrintStack(stackTrace: s);
+      throw ServerException(
+        message: e.toString(),
+        statusCode: '505',
+      );
+    }
   }
 
   Future<DataMap> _getUserData(String uid) async {
@@ -77,13 +204,22 @@ class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
         as DataMap;
   }
 
-  Future<void> _setUserData(User user, String fallbackEmail) async{
-    final userMap = {
-      'uid': user.id,
-      'email': user.email ?? fallbackEmail,
-      'name': user. ?? '',
-      'profilePic': user.photoURL ?? '',
-      'points': 0,
-    };
+  Future<void> _setUserData(user, String fallbackEmail) async {
+    await _client.from('users').upsert({
+      LocalUserModel(
+        uid: user.id.toString(),
+        name: user.userMetadata!.name.toString() ?? '',
+        email: user.email.toString() ?? fallbackEmail,
+        profilePic: user.userMetadata!.photoUrl.toString() ?? '',
+        points: 0,
+      ).toMap(),
+    });
+  }
+
+  Future<void> _updateUserData(DataMap data) async {
+    await _client
+        .from('users')
+        .update(data)
+        .eq('uid', _client.auth.currentUser?.id);
   }
 }
