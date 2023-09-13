0fb551ed12025bdc39c30bec759377eba8d836bf
Bishal <bishalbera2@gmail.com>
2023-09-13 15:24:25 +0530

    coursemodel created of the data layer

---
+++ lib/core/common/features/course/data/models/course_model.dart
@@ -0,0 +1,83 @@
+import 'package:skillify/core/common/features/course/domain/entities/course.dart';
+import 'package:skillify/core/utils/typedef.dart';
+
+class CourseModel extends Course {
+  const CourseModel({
+    required super.id,
+    required super.title,
+    required super.numberOfExams,
+    required super.numberOfMaterials,
+    required super.numberOfVideos,
+    required super.groupId,
+    required super.updatedAt,
+    required super.createdAt,
+    super.description,
+    super.image,
+    super.imageIsFile = false,
+  });
+
+  CourseModel.empty()
+      : this(
+          id: '_empty.id',
+          title: '_empty.title',
+          description: '_empty.description',
+          numberOfExams: 0,
+          numberOfMaterials: 0,
+          numberOfVideos: 0,
+          groupId: '_empty.groupId',
+          createdAt: DateTime.now(),
+          updatedAt: DateTime.now(),
+        );
+
+  CourseModel.fromMap(DataMap map)
+      : super(
+          id: map['id'] as String,
+          title: map['title'] as String,
+          description: map['description'] as String?,
+          groupId: map['groupId'] as String,
+          numberOfExams: (map['numberOfExams'] as num).toInt(),
+          numberOfMaterials: (map['numberOfMaterials'] as num).toInt(),
+          numberOfVideos: (map['numberOfVideos'] as num).toInt(),
+          image: map['image'] as String?,
+          createdAt: DateTime.parse(map['createdAt'] as String),
+          updatedAt: DateTime.parse(map['updatedAt'] as String),
+        );
+
+  CourseModel copyWith({
+    String? id,
+    String? title,
+    String? description,
+    String? groupId,
+    int? numberOfVideos,
+    int? numberOfExams,
+    int? numberOfMaterials,
+    String? image,
+    bool? imageIsFile,
+    DateTime? createdAt,
+    DateTime? updatedAt,
+  }) {
+    return CourseModel(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      numberOfExams: numberOfExams ?? this.numberOfExams,
+      numberOfMaterials: numberOfMaterials ?? this.numberOfMaterials,
+      numberOfVideos: numberOfVideos ?? this.numberOfVideos,
+      groupId: groupId ?? this.groupId,
+      updatedAt: updatedAt ?? this.updatedAt,
+      createdAt: createdAt ?? this.createdAt,
+    );
+  }
+
+  DataMap toMap() => {
+        'id': id,
+        'title': title,
+        'description': description,
+        'groupId': groupId,
+        'image': image,
+        'createdAt': DateTime.now().toUtc().toIso8601String(),
+        'updatedAt': DateTime.now().toUtc().toIso8601String(),
+        'numberOfVideos': numberOfVideos,
+        'numberOfExams': numberOfExams,
+        'numberOfMaterials': numberOfMaterials,
+      };
+}
