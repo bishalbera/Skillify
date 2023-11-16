853ef5a20c36ec60475eef940fc7f437b6d50524
Bishal <bishalbera2@gmail.com>
2023-11-16 11:10:13 +0530

    localuser model created

--- fastapi/app/models/users.py
+++ fastapi/app/models/users.py
@@ -1,2 +1,18 @@
-from .database import Base
-class LocalUser(Base) 
+from ..database import Base
+from sqlalchemy import Column, Integer, String, ARRAY, ForeignKey
+
+class LocalUser(Base):
+  __tablename__ = "users"
+  
+  id = Column(Integer, primary_key= True, nullable= False)
+  name = Column(String, nullable= False)
+  email = Column(String, nullable= False)
+  password = Column(String, nullable=False)
+  points = Column(Integer, nullable= False)
+  bio = Column(String, nullable= True)
+  profile_pic = Column(String, nullable= True)
+  group_ids = Column(ARRAY)
+  enrolled_course_ids = Column(ARRAY)
+  followings = Column(ARRAY)
+  followers = Column(ARRAY)
+
